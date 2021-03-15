d22d6f2a72556ee6a54044faeefcc4c210327c41
Ian Riley <ian.s.riley@outlook.com>
2021-03-15 17:41:52 -0600

    api reset

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,10 +3,10 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_appsync_graphqlEndpoint": "https://ozrhlul43battcpp5jktgl4ipa.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://4pmrbwdarndhpn5udgklgl4uza.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-jrr4kga53bapjjz5nydq7nagwq",
+    "aws_appsync_apiKey": "da2-pqhmm77aqzdsda25dalok5sw7a",
     "aws_cognito_identity_pool_id": "us-east-1:c7e1e1b9-6da3-4c53-a44c-5cb0a48529c4",
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_PsV3obwoO",
